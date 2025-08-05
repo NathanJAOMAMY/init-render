7038b16d77e161c1279918d6630680df4dda5b32
NathanJAOMAMY <nathanjaomamy@gmail.com>
2025-08-05 16:07:18 +0300

    new route

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.send('🚀 API en ligne sur Heroku !');
 });
 
+// Route principale
+app.get('/test', (req, res) => {
+  res.json({data : "Test de l'API Heroku réussi !"});
+});
+
 // Utiliser le port Heroku ou 3000 en local
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Serveur démarré sur le port ${PORT}`));
